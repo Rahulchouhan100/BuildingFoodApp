d5246595c62d8621256d5af50513ddd388e7552b
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2023-01-23 22:15:27 +0530

    WorkingOnMenu

--- src/Component/RestMenu/RestaurantMenu.js
+++ src/Component/RestMenu/RestaurantMenu.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { IMG_URL } from "../../../config.js";
+import "./RestMenu.css";
+import starIcon from "../../assets/star-1.png";
+// import { BsFillStarFill } from "react-icons/Bs";
+// import { AiFillStar } from "react-icons/Ai";
 
 const RestaurantMenu = () => {
   const [restaurant, setRestaurant] = useState({});
@@ -29,10 +33,42 @@ const RestaurantMenu = () => {
   return !restaurant ? (
     "loading"
   ) : (
-    <div>
-      <h1>this is ResMenu {id} </h1>
-      <p>{restaurant.name} </p>
-      <img src={IMG_URL + restaurant.cloudinaryImageId} alt="" />
+    <div className="menu-container">
+      <div>
+        <img
+          src={IMG_URL + restaurant.cloudinaryImageId}
+          alt=""
+          className="menu-img"
+        />
+        <div>
+          <h2>{restaurant.name}</h2>
+          <h4>{restaurant.cuisines}</h4>
+          <h4>
+            {restaurant.area} , {restaurant.city}
+          </h4>
+          <div>
+            <div>
+              <span>
+                {" "}
+                <img src={starIcon} alt="" className="star-icon" />{" "}
+              </span>{" "}
+              <span className="rating">{restaurant.avgRatingString} </span>
+              <br />
+              {restaurant.totalRatingsString}
+            </div>
+            <div>
+              {restaurant.sla?.slaString} {restaurant?.badges?.costForTwoMsg}
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>offer</div>
+      </div>
+
+      {/* <h1>this is ResMenu {id} </h1>
+      <p>{restaurant.name} </p> */}
     </div>
   );
 };
