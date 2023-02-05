1f7ed58e1e24ab24b2a508227536da8c279a4448
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2023-02-05 09:51:44 +0530

    -

--- src/Component/AboutUs/About.js
+++ src/Component/AboutUs/About.js
@@ -1,9 +1,26 @@
 import "./About.css";
-const About = () =>{
-    return(
-        <>
-           <h1>Hi I'm About</h1>
-        </>
-    )
-}
+
+const Section = ({ title, para }) => {
+  return (
+    <>
+      <div>
+        <h3>{title}</h3>
+        <p>{para}</p>
+      </div>
+    </>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <h1>this is heading !!!</h1>
+      <Section
+        title={"About Onifoods"}
+        para={"we're good at food delivering"}
+      />
+      <Section title={"Team Onifoods"} para={"we're good at team works"} />
+    </>
+  );
+};
 export default About;
