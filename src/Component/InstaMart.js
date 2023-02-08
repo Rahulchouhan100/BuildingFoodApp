f0901f479cb46f0e4a0f7516efe2bf97637911d0
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2023-02-08 14:29:36 +0530

    Accordian built but not in good way

--- src/Component/InstaMart.js
+++ src/Component/InstaMart.js
@@ -1,64 +1,106 @@
-import { useState } from "react";
 import "./instamart.css";
+import ArrowDown from "../assets/arrowDown.png";
+import ArrowUp from "../assets/arrowUp.png";
+import { useState } from "react";
 
-const Section = ({ title, description }) => {
-  const [isVisible, setIsvisible] = useState(false);
-  //  I need to give control to the instamart
-  // taaki my parent can control the children ( this is known as lifting the state up)
+const Section = ({ title, description, isVisible, setIsVisible }) => {
+  // const [isVisible, setIsVisible] = useState(false);
   return (
     <>
-      <div className="instamart">
-        <h1>{title}</h1>
-        {isVisible ? (
-          <button
-            onClick={() => {
-              setIsvisible(false);
-            }}
-          >
-            Hide
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              setIsvisible(true);
-            }}
-          >
-            Show
-          </button>
-        )}
-        {isVisible && <h4>{description}</h4>}
+      <div className="instamart-container">
+        <div>
+          <div className="title-container">
+            <h2>{title}</h2>
+
+            {isVisible ? (
+              <button onClick={() => setIsVisible(false)}>
+                <img src={ArrowUp} alt="arrow" className="arrowDown" />
+              </button>
+            ) : (
+              <button onClick={() => setIsVisible(true)}>
+                <img src={ArrowDown} alt="arrow" className="arrowDown" />
+              </button>
+            )}
+          </div>
+          {isVisible && <p>{description}</p>}
+        </div>
       </div>
     </>
   );
 };
 
-const InstaMart = () => {
+const Instamart = () => {
+  const [config, setConfig] = useState({
+    editOrder: true,
+    cancelOrder: false,
+    valueOrder: false,
+    invoiceOrder: false,
+  });
   return (
     <>
-      <h1>hello I'm InstaMart</h1>
+      <h1>Instamart FAQS</h1>
       <Section
-        title={"About instamart"}
+        title={"Can I edit my order?"}
         description={
-          " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis numquam harum blanditiis soluta distinctio incidunt maiores debitis, quasi quia cumque nobis velit quidem dolor aliquid pariatur praesentium officiis. Voluptas dolores eaque quas earum blanditiis odit iure eligendi aspernatur ipsum quam nisi optio enim nobis repudiandae, nostrum qui impedit? Praesentium, deserunt.        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis numquam harum blanditiis soluta distinctio incidunt maiores debitis, quasi quia cumque nobis velit quidem dolor aliquid pariatur praesentium officiis. Voluptas dolores eaque quas earum blanditiis odit iure eligendi aspernatur ipsum quam nisi optio enim nobis repudiandae, nostrum qui impedit? Praesentium, deserunt."
+          "Your order can be edited before it reaches the restaurant. You could contact customer support team via chat or call to do so. Once order is placed and restaurant starts preparing your food, you may not edit its contents"
         }
+        isVisible={config.editOrder}
+        setIsVisible={() => {
+          setConfig({
+            editOrder: true,
+            cancelOrder: false,
+            valueOrder: false,
+            invoiceOrder: false,
+          });
+        }}
       />
       <Section
-        title={"Career Instamart"}
+        title={"I want to cancel my order"}
         description={
-          " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis numquam harum blanditiis soluta distinctio incidunt maiores debitis, quasi quia cumque nobis velit quidem dolor aliquid pariatur praesentium officiis. Voluptas dolores eaque quas earum blanditiis odit iure eligendi aspernatur ipsum quam nisi optio enim nobis repudiandae, nostrum qui impedit? Praesentium, deserunt.        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis numquam harum blanditiis soluta distinctio incidunt maiores debitis, quasi quia cumque nobis velit quidem dolor aliquid pariatur praesentium officiis. Voluptas dolores eaque quas earum blanditiis odit iure eligendi aspernatur ipsum quam nisi optio enim nobis repudiandae, nostrum qui impedit? Praesentium, deserunt."
+          "We will do our best to accommodate your request if the order is not placed to the restaurant (Customer service number:  080-67466729). Please note that we will have a right to charge a cancellation fee up to full order value to compensate our restaurant and delivery partners if your order has been confirmed."
         }
+        isVisible={config.cancelOrder}
+        setIsVisible={() => {
+          setConfig({
+            editOrder: false,
+            cancelOrder: true,
+            valueOrder: false,
+            invoiceOrder: false,
+          });
+        }}
       />
       <Section
-        title={"Contact Instamart"}
+        title={"Is there a minimum order value?"}
         description={
-          "           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis numquam harum blanditiis soluta distinctio incidunt maiores debitis, quasi quia cumque nobis velit quidem dolor aliquid pariatur praesentium officiis. Voluptas dolores eaque quas earum blanditiis odit iure eligendi aspernatur ipsum quam nisi optio enim nobis repudiandae, nostrum qui impedit? Praesentium, deserunt.     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis numquam harum blanditiis soluta distinctio incidunt maiores debitis, quasi quia cumque nobis velit quidem dolor aliquid pariatur praesentium officiis. Voluptas dolores eaque quas earum blanditiis odit iure eligendi aspernatur ipsum quam nisi optio enim nobis repudiandae, nostrum qui impedit? Praesentium, deserunt."
+          "We have no minimum order value and you can order for any amount. "
         }
+        isVisible={config.valueOrder}
+        setIsVisible={() => {
+          setConfig({
+            editOrder: false,
+            cancelOrder: false,
+            valueOrder: true,
+            invoiceOrder: false,
+          });
+        }}
+      />
+      <Section
+        title={"I want an invoice for my order"}
+        description={"send an email. We will revert within 24-48 hrs"}
+        isVisible={config.invoiceOrder}
+        setIsVisible={() => {
+          setConfig({
+            editOrder: false,
+            cancelOrder: false,
+            valueOrder: false,
+            invoiceOrder: true,
+          });
+        }}
       />
     </>
   );
 };
-export default InstaMart;
-
+export default Instamart;
 // suppose this is a very big component and it has 1000 of components inside it.
 
 // chapter 11 data is new oil
