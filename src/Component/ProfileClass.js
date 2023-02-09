8f3d9d8861459a357996281a13c3527c723cc335
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2023-02-09 12:34:30 +0530

    classBased

--- src/Component/ProfileClass.js
+++ src/Component/ProfileClass.js
@@ -1,12 +1,29 @@
 // we are going to write class based component
 // render method return some jsx
+// we can manage state like this : -
 import React from "react";
 
 class ProfileClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
   render() {
-    <>
-      <h1>This is Profile class component</h1>
-    </>;
+    const { count } = this.state;
+    return (
+      <>
+        <h1>This is Profile class component</h1>
+        <h2>{count}</h2>
+        <button
+          onClick={() => {
+            this.setState({ count1: 1 });
+          }}
+        ></button>
+      </>
+    );
   }
 }
 
